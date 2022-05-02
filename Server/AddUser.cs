bfa3fc268c39ff710cbf817cae8949b9e60ea4d7
Susidko Vladislav <vladislavsus7@gmail.com>
2022-05-02 20:46:51 +0300

    User added. User update.

---
+++ Server/AddUser.cs
@@ -0,0 +1,169 @@
+﻿using DALTestSystem;
+using RepositoryLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class AddUser : Form
+    {
+        GenericUnitOfWork work = new GenericUnitOfWork(new TestUserContext(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString));
+        IGenericRepository<User> repositoryUser;
+        IGenericRepository<Group> repositoryGroup;
+        List<string> logins = new List<string>();
+        List<Group> groups = new List<Group>();
+        User user;
+        bool isEdit = false;
+        public AddUser(User user, bool isEdit)
+        {
+            InitializeComponent();
+            this.isEdit = isEdit;
+            this.user = user;
+            repositoryUser = work.Repository<User>();
+            repositoryGroup = work.Repository<Group>();
+            getAllGroups();
+            if (isEdit)
+            {
+                editUserLoad();
+                //this.user = new User();
+                //this.user.Id = user.Id;
+            }
+            logins = repositoryUser.GetAll().ToList().Select(x => x.Login).ToList();
+        }
+
+        //Заповнення списку груп
+        private void getAllGroups()
+        {
+            groups = repositoryGroup.GetAll().ToList();
+            foreach (Group item in groups)
+            {
+                checkedListBox1.Items.Add(item.Name);
+            }
+        }
+
+        private void editUserLoad()
+        {
+            textBox1.Text = user.FirstName;
+            textBox2.Text = user.LastName;
+            textBox3.Text = user.Login;
+            textBox4.Text = user.Password;
+            checkBox1.Checked = user.isAdmin;
+            if(user.Groups.Count > 0)
+            {
+                foreach (Group item in user.Groups)
+                {
+                    for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                    {
+                        if(item.Name == checkedListBox1.Items[i].ToString())
+                        {
+                            checkedListBox1.SetItemChecked(i, true);
+                        }
+                    }
+                }
+            }
+        }
+
+        //Показати пароль
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox2.CheckState == CheckState.Checked)
+            {
+                textBox4.PasswordChar = '\0';
+            }
+            else
+            {
+                textBox4.PasswordChar = '*';
+            }
+        }
+
+        //Відмінити
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Додати користувача
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox3.ForeColor == Color.Red)
+            {
+                MessageBox.Show("This login has already been used!", "", MessageBoxButtons.OK);
+                return;
+            }
+            else if (textBox1.Text == " " || textBox2.Text == " " || textBox3.Text == " " || textBox4.Text == " ")
+            {
+                MessageBox.Show("Incorrect data", "", MessageBoxButtons.OK);
+                return;
+            }
+
+            user.FirstName = textBox1.Text;
+            user.LastName = textBox2.Text;
+            user.Login = textBox3.Text;
+            user.Password = textBox4.Text;
+            user.isAdmin = checkBox1.Checked;
+            List<Group> groups = new List<Group>();
+            user.Groups.Clear();
+            if (checkedListBox1.CheckedItems.Count > 0)//Заповнення груп користувача (якщо вибрані)
+            {
+                foreach (string item in checkedListBox1.CheckedItems)
+                {
+                    Group group = repositoryGroup.FindAll(x => x.Name == item).FirstOrDefault();
+                    groups.Add(group);
+                }
+            }
+            user.Groups = groups;
+            if (!isEdit)
+            {
+                repositoryUser.Add(user);
+                work.SaveChanges();
+            }
+            else
+            {
+                repositoryUser.Update(user);
+                work.SaveChanges();
+            }
+            work.Dispose();
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+ 
+
+        //Перевірка чи можна використовувати цей логін
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (!isEdit)
+            {
+                if (logins.Contains(textBox3.Text))
+                {
+                    textBox3.ForeColor = Color.Red;
+                }
+                else
+                {
+                    textBox3.ForeColor = Color.Black;
+                }
+            }
+            else
+            {
+                if(logins.Contains(textBox3.Text) && textBox3.Text != user.Login)
+                {
+                    textBox3.ForeColor = Color.Red;
+                }
+                else
+                {
+                    textBox3.ForeColor = Color.Black;
+                }
+            }
+            
+        }
+
+    }
+}
--- Server/Form1.cs
+++ Server/Form1.cs
@@ -25,6 +25,7 @@ namespace Server
             repositoryGroup = work.Repository<Group>();
         }
 
+        //Перевірка входу на сервер
         private void Form1_Load(object sender, EventArgs e)
         {
             StringBuilder log = new StringBuilder();
@@ -36,22 +37,19 @@ namespace Server
                 string lg = log.ToString();
                 string ps = pass.ToString();
                 User user = repositoryUser.FindAll(u => u.Login == lg && u.Password == ps).FirstOrDefault();
-                if(user != null)
-                {
-                    
-                }
-                else
+                if(user == null || user.isAdmin == false)
                 {
                     this.Close();
                 }
             }
+            else
+            {
+                this.Close();
+            }
         }
 
-        private void Users_Click(object sender, EventArgs e)
-        {
 
-        }
-
+        //Перехід між вкладками 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
             switch (tabControl1.SelectedTab.Text)
@@ -60,11 +58,19 @@ namespace Server
 
                     break;
                 case "Users":
+                    dataGridView1.Rows.Clear();
+                    dataGridView2.Rows.Clear();
                     List<User> Users = new List<User>();
                     Users = repositoryUser.GetAll().ToList();
                     foreach (User item in Users)
                     {
                         dataGridView1.Rows.Add(item.Id, item.FirstName, item.LastName, item.Login, item.Password, item.isAdmin);
+                    }
+                    User currentUser = Users[0];
+                    List<Group> groups = currentUser.Groups.ToList();
+                    foreach (Group item in groups)
+                    {
+                        dataGridView2.Rows.Add(item.Id, item.Name);
                     }
                     break;
                 case "Groups":
@@ -77,6 +83,75 @@ namespace Server
 
                     break;
             }
+        }
+
+        //Додати користувача
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            AddUser addUser = new AddUser(user, false);
+            DialogResult result = addUser.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                dataGridView1.Rows.Add(user.Id, user.FirstName, user.LastName, user.Login, user.Password, user.isAdmin);
+            }
+        }
+
+        //Підвантаження груп в яких є користувач
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                dataGridView2.Rows.Clear();
+                User user = repositoryUser.FindById(dataGridView1.CurrentRow.Cells[0].Value);
+                foreach (Group item in user.Groups)
+                {
+                    dataGridView2.Rows.Add(item.Id, item.Name);
+                }
+            }
+        }
+
+        //Редагувати користувача
+        private void button2_Click(object sender, EventArgs e)
+        {
+            User user = repositoryUser.FindById(dataGridView1.CurrentRow.Cells[0].Value);
+            user.LastName = "SUSID";
+            List<Group> groups = new List<Group>();
+            foreach (Group item in user.Groups)
+            {
+                groups.Add(item);
+            }
+            user.Groups.Clear();
+            user.Groups = groups;
+            work.SaveChanges();
+
+
+
+
+
+
+            //user.Id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            //user.FirstName = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            //user.LastName = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            //user.Login = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            //user.Password = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            //user.isAdmin = Convert.ToBoolean(dataGridView1.CurrentRow.Cells[5].Value);
+            //if(dataGridView2.Rows.Count > 0)
+            //{
+            //    foreach (DataGridViewRow item in dataGridView2.Rows)
+            //    {
+            //        Group group = repositoryGroup.FindById(item.Cells[0].Value);
+            //        user.Groups.Add(group);
+            //    }
+            //}
+            //AddUser addUser = new AddUser(user, true);
+            //DialogResult result = addUser.ShowDialog();
+            //if(result == DialogResult.OK)
+            //{
+               // repositoryUser.Update(user);
+                //work.SaveChanges();
+            //}
+
         }
     }
 }
